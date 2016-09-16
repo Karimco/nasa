18464f74382f150c7e6f698176c0a87e8003ba8d
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2016-09-16 10:40:58 +0200

    make data recuperation ok

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 	var apiKey = "U31QXBjycdeRrJ0gsnCQpvlaWLpvBBsb2QKCQqUL";
 
 	var startDate = "2016-09-16";
-	var endDate = "2016-09-16";
+	var endDate = "2016-09-17";
 
 	var url = "https://api.nasa.gov/neo/rest/v1/feed?api_key="+apiKey+"&start_date="+startDate+"&end_date="+endDate;
 
@@ -15,24 +15,34 @@ $(document).ready(function() {
 
 			console.log(result);
 
-			for(let i in result.near_earth_objects[startDate]) {
+			for(let i in result.near_earth_objects[endDate]) {
 
-				let asteroid = result.near_earth_objects[startDate][i];
+				let asteroid = result.near_earth_objects[endDate][i];
 
 				console.log(asteroid);
 				console.log(asteroid.name);
 
-				// let test = asteroid.absolute_magnitude_h;
+				let hazardous = "Oui";
+
+				if(!asteroid.is_potentially_hazardous_asteroid) hazardous = "Non";
+
+				let missDistance = asteroid.close_approach_data[0].miss_distance.kilometers;
+				let timestamp = moment(asteroid.close_approach_data[0].epoch_date_close_approach);
+				let diameter = 
+					Math.round((asteroid.estimated_diameter.meters.estimated_diameter_max + asteroid.estimated_diameter.meters.estimated_diameter_min) /2)
+				;
 
 				let element = `
 					<li>
-						test
+						<div>name : ${asteroid.name}
+						<div>hazardous : ${hazardous}</div>
+						<div>Hour Pass : ${timestamp}</div>
+						<div>Miss distance : ${missDistance} kilometers</div>
+						<div>Diameter : ${diameter} meters</div>
 					</li>
 				`;
 
 				$('ul').append(element);
-
-
 			}
 		}
 	});
