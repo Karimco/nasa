6899a05337e3e373783927b02b1d433928babbbe
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2016-09-16 10:22:09 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+"use strict";
+
+$(document).ready(function() {
+
+	var apiKey = "U31QXBjycdeRrJ0gsnCQpvlaWLpvBBsb2QKCQqUL";
+
+	var startDate = "2016-09-16";
+	var endDate = "2016-09-16";
+
+	var url = "https://api.nasa.gov/neo/rest/v1/feed?api_key="+apiKey+"&start_date="+startDate+"&end_date="+endDate;
+
+	$.ajax({
+		url: url,
+		success: function(result) {
+
+			console.log(result);
+
+			for(let i in result.near_earth_objects) {
+
+				let asteroid = result.near_earth_objects[i];
+
+				console.log(asteroid);
+
+				let test = asteroid.absolute_magnitude_h;
+
+				let element = `
+					<li>
+						test
+					</li>
+				`;
+
+				$('ul').append(element);
+			}
+		}
+	});
+});
