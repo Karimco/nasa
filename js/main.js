f1f379eb72bf598c43ee9c4d1352232ebd8c274d
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2016-09-16 12:15:27 +0200

    pass config apikey to json hidden

--- js/main.js
+++ js/main.js
@@ -6,14 +6,11 @@ $(document).ready(function() {
 	/*	  Variables    */
 	/*******************/
 
-	var apiKey = "U31QXBjycdeRrJ0gsnCQpvlaWLpvBBsb2QKCQqUL",
-		startDate = "2016-09-16", //moment().format("YYYY-MM-DD");
-		endDate = "2016-09-17",
-		url = "https://api.nasa.gov/neo/rest/v1/feed?api_key="+apiKey+"&start_date="+startDate+"&end_date="+endDate
+	let startDate = "2016-09-16", //moment().format("YYYY-MM-DD");
+		endDate = "2016-09-17"
 	;
 
 	const launchAtBegin = false;
-
 
 
 
@@ -22,15 +19,33 @@ $(document).ready(function() {
 	/*	  Launching    */
 	/*******************/
 
-	// Launch watching of Asteroids
-	brucewillis(url);
+	checkHour();
 
+	getConfiguration(function(apiKey) {
+
+		const url = "https://api.nasa.gov/neo/rest/v1/feed?api_key="+apiKey+"&start_date="+startDate+"&end_date="+endDate;
+
+		// Launch watching of Asteroids
+		brucewillis(url);
+	});
 
 
 
 	/*******************/
 	/*	  Functions    */
 	/*******************/
+
+	function getConfiguration(callback) {
+		$.ajax({
+			url: 'config.json',
+			method: 'get',
+			success: function(response) {
+				console.log(response);
+
+				callback(response.apikey);
+			}
+		})
+	}
 
     // Get les datas asteroid du jour
 	function brucewillis(url) {
@@ -69,7 +84,7 @@ $(document).ready(function() {
 
 					let element = `
 						<li>
-							<div>name : ${object.name}
+							<div><b>${object.name}</b></div>
 							<div>hazardous : ${object.hazardous}</div>
 							<div>Hour Pass : ${object.timestamp}</div>
 							<div>Miss distance : ${object.missDistance} kilometers</div>
@@ -93,7 +108,7 @@ $(document).ready(function() {
 
     	for(let elem in asteroids) {
     		if(asteroids[elem].timestamp == moment().format("HH:mm")) {
-    			impact(asteroids[elem]);	
+    			impact(asteroids[elem].name);	
     		}
     	}
 
@@ -101,7 +116,7 @@ $(document).ready(function() {
 
         	for(let elem in asteroids) {
         		if(asteroids[elem].timestamp == moment().format("HH:mm")) {
-        			impact(asteroids[elem]);	
+        			impact(asteroids[elem].name);	
         		}
         	}
 
@@ -109,9 +124,9 @@ $(document).ready(function() {
     }
 
     // LittleBit
-	function impact(asteroid) {
+	function impact(asteroidName) {
 
-		$('.showImpact').text(asteroid.name + " is passing next to us");
+		$('.showImpact').text(asteroidName + " is passing next to us");
 
 		const makerKey = "jBdzjx6A04qjewchH_aRTAnKz_838X8VntQ4qCPay2Z",
 			url = "https://maker.ifttt.com/trigger/asteroid/with/key/"+makerKey
@@ -126,7 +141,14 @@ $(document).ready(function() {
         });
 	}
 
+	function checkHour() {
+
+		setInterval(() => {
+			$('.hour').text(moment().format("HH:mm:ss"));
+		}, 1000);
+	}
+
 	$('.impact').on('click', () => {
-		impact();
+		impact("test");
 	});
 });
