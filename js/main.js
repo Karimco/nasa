0db5f014e883e4a2d8da6501baef16c87591d2cc
ECVD-PA-Q084-IM019 <ECVD-IM019@ECVD-PA-Q084-IM019.local>
2016-09-16 11:17:16 +0200

    divers

--- js/main.js
+++ js/main.js
@@ -4,13 +4,14 @@ $(document).ready(function() {
 
 	var apiKey = "U31QXBjycdeRrJ0gsnCQpvlaWLpvBBsb2QKCQqUL";
 
-	var startDate = "2016-09-16";
+	var startDate = "2016-09-16";//moment().format("YYYY-MM-DD");
 	var endDate = "2016-09-17";
 
 	var url = "https://api.nasa.gov/neo/rest/v1/feed?api_key="+apiKey+"&start_date="+startDate+"&end_date="+endDate;
 
 	brucewillis(url);
 
+    //Get les datas asteroid du jour
 	function brucewillis(url) {
 
 		$.ajax({
@@ -31,7 +32,7 @@ $(document).ready(function() {
 						name: asteroid.name,
 						hazardous: "Oui",
 						missDistance: asteroid.close_approach_data[0].miss_distance.kilometers,
-						timestamp: moment(asteroid.close_approach_data[0].epoch_date_close_approach),
+						timestamp: moment(asteroid.close_approach_data[0].epoch_date_close_approach).format("HH:mm"),
 						diameter: Math.round((asteroid.estimated_diameter.meters.estimated_diameter_max + asteroid.estimated_diameter.meters.estimated_diameter_min) /2)
 					}
 
@@ -48,7 +49,7 @@ $(document).ready(function() {
 							<div>Diameter : ${object.diameter} meters</div>
 						</li>
 					`;
-
+                    
 					functionKarimjy(asteroids);
 
 					$('ul').append(element);
@@ -56,8 +57,32 @@ $(document).ready(function() {
 			}
 		});
 	}
+    
+    //Check le timestamp asteroid à maintenant
+    function check(res){
+        setInterval(function(){
+                if(res[0].timestamp == moment().format("HH:mm")){
+                    return true;
+                }
+        }, 1000*60);
+    }
+    
+    //Lance la music
+    function letsgo(){
+        
+    }
 
+    //LittleBit
 	function functionKarimjy(asteroids) {
-		console.log(asteroids);
+        var url = "";
+            $.ajax({
+                url: url,
+                method: 'get',
+                success: function(result) {
+                    let boolean = check(result);
+                    
+                    if(boolean){ letsgo() }
+                }
+            });
 	}
 });
